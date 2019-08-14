db812687c43f6648890eacd052e360de7979a9f2
Flionic <flionicj@gmail.com>
2019-08-15 02:40:53 +0300

    Bootstrap connected!

--- adddn.py
+++ adddn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.7
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, instance_relative_config=True)
@@ -10,12 +10,12 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 app.url_map.strict_slashes = False
 app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config['APP_NAME'] = 'rndsite'
-app.config['APP_TITLE'] = 'Randomize WebSite'
+app.config['APP_NAME'] = 'adddn'
+app.config['APP_TITLE'] = 'Add domains'
 app.config['VERSION'] = '0.0.1'
 app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY', '7-DEV_MODE_KEY-7')
 
 
 @app.route('/')
 def index():
-    return 'hello!'
+    return render_template('index.html')
