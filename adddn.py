0b3ef61916c53d935270f336c39f19dcadf075ae
Flionic <flionicj@gmail.com>
2019-08-15 02:27:21 +0300

    Hello Word with wsgi

---
+++ adddn.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3.7
+import os
+
+from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
+
+app = Flask(__name__, instance_relative_config=True)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.config['APP_NAME'] = 'rndsite'
+app.config['APP_TITLE'] = 'Randomize WebSite'
+app.config['VERSION'] = '0.0.1'
+app.config['SECRET_KEY'] = os.getenv('APP_SECRET_KEY', '7-DEV_MODE_KEY-7')
+
+
+@app.route('/')
+def index():
+    return 'hello!'
