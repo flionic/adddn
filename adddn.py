181e57c37225b6f2b730877c7ef7a2c73034ba18
Flionic <flionicj@gmail.com>
2019-08-15 06:54:04 +0300

    Password protection: add sessions, login manager

--- adddn.py
+++ adddn.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3.7
+import bcrypt
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, jsonify, request
+from flask_login import LoginManager, logout_user, login_user, current_user
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.contrib.fixers import ProxyFix
 
@@ -21,6 +24,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_link
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.config['SESSION_TYPE'] = 'redis'
+sess = Session(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "users.login"
+
+
+@login_manager.user_loader
+def load_user(uid):
+    return Settings.query.filter_by(key=uid).first()
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    flash('Для этого действия требуется авторизация', 'error')
+    return redirect(url_for('index'))
 
 
 class Settings(db.Model):
@@ -31,6 +48,22 @@ class Settings(db.Model):
         self.key = key
         self.value = value
 
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    @staticmethod
+    def get_id():
+        return 'username'
+
     def __repr__(self):
         return "'%s': '%s'" % (self.key, self.value)
 
@@ -38,6 +71,27 @@ class Settings(db.Model):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if current_user.is_authenticated:
+        flash('Вы уже авторизированы', 'warning')
+        return redirect(url_for('index'))
+    password = Settings.query.filter_by(key='password').first()
+    # elif bcrypt.checkpw(str.encode(request.form['password']), str.encode(password.value)) is False:
+    if bcrypt.checkpw(str.encode(request.form['password']), str.encode(password.value)) is False:
+        flash('Неверный пароль', 'error')
+    else:
+        login_user(Settings.query.filter_by(key='username').first())
+        flash(f'Авторизирован')
+    return jsonify({'response': 1})
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 def init_app():
