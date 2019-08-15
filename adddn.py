aa0097f521ff602e63121484810c9a63d0dfae2a
Flionic <flionicj@gmail.com>
2019-08-15 06:32:48 +0300

    Redis coming

--- adddn.py
+++ adddn.py
@@ -20,6 +20,7 @@ db_link = 'sqlite:///' + os.path.join(basedir, 'main.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = db_link
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['SESSION_TYPE'] = 'redis'
 
 
 class Settings(db.Model):
